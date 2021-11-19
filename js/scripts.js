9a6654af62b5f5558050e17dfbe7facb6b7777f2
chrisdfghjkl <cdepastene@gmail.com>
2021-11-18 18:11:59 -0800

    first test function - passes

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Business Logic
+
+function roboger(number) {
+  let resultArray = [];
+  
+  for (let i = 0; i <= number; i++) {
+    resultArray.push(i);
+  }
+  return resultArray;
+}
+roboger(0)
