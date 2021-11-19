d5de42f6eab6c24cbac71dce48efd82e8f8f1cc3
chrisdfghjkl <cdepastene@gmail.com>
2021-11-18 18:57:23 -0800

    Test attempt - function meets prompt requirements

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 // Business Logic
 
 function roboger(number) {
+  if (number < 0) {
+    return "You Broke It!";
+  }
+  
   let resultArray = [];
   
   for (let i = 0; i <= number; i++) {
-    if (i.toString().indexOf(1) > -1) {
-      resultArray.push("TEST");
+    if (i.toString().indexOf(3) > -1) {
+      resultArray.push("Won't you be my neighbor?");
+    } else if (i.toString().indexOf(2) > -1) {
+      resultArray.push("Boop!");
+    } else if (i.toString().indexOf(1) > -1) {
+      resultArray.push("Beep!");
     } else {
       resultArray.push(i);
     }
   }
 return resultArray;
 }
-roboger(5)
+roboger(13)
